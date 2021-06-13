79fca187f14a3ab4c2bc2aaf253fa194965acacd
Jared <jcrean01@gmail.com>
2021-06-13 15:34:04 -0400

    add tests for polynomial derivatives

--- include/utils/legendre.h
+++ include/utils/legendre.h
@@ -11,15 +11,15 @@ class LegendrePoly
   using Index = int;
 
   public:
-    Real evalPoly(const Index degree, const Real x) { return recurrence(degree, x, 1.0, x); }
+    Real evalPoly(const Index degree, const Real x) { return recurrence(degree, x); }
     
-    Real evalPolyDeriv(const Index degree, const Real x) { return recurrence(degree, x, 0.0, 1.0); }
+    Real evalPolyDeriv(const Index degree, const Real x) { return recurrence_dx(degree, x); }
 
   private:  
-    Real recurrence(const Index degree, const Real x, const Real seed0, const Real seed1)
+    Real recurrence(const Index degree, const Real x)
     {
-      Real p_n2 = seed0;
-      Real p_n1 = seed1;
+      Real p_n2 = 1.0;
+      Real p_n1 = x;
 
       if (degree == 0)
         return p_n2;
@@ -36,6 +36,30 @@ class LegendrePoly
       }
 
       return p_n;
+    }
+
+    Real recurrence_dx(const Index degree, const Real x)
+    {
+      Real p_n2 = 1.0, p_n2_dx = 0.0;
+      Real p_n1 = x,   p_n1_dx = 1.0;
+
+      if (degree == 0)
+        return p_n2_dx;
+
+      if (degree == 1)
+        return p_n1_dx;
+
+      Real p_n, p_n_dx;
+      for (int n=2; n <= degree; ++n)
+      {
+        p_n    = ((2*n - 1)*x*p_n1 - (n - 1)*p_n2)/n;
+        p_n_dx = ((2*n - 1)*(p_n1 + x*p_n1_dx) - (n - 1)*p_n2_dx)/n;
+
+        p_n2 = p_n1; p_n2_dx = p_n1_dx;
+        p_n1 = p_n;  p_n1_dx = p_n_dx;
+      }
+
+      return p_n_dx;
     }
 };
 
--- test/test_polynomials.cc
+++ test/test_polynomials.cc
@@ -1,6 +1,7 @@
 #include <cmath>
 
 #include "gtest/gtest.h"
+#include "utils/lagrange.h"
 #include "utils/legendre.h"
 #include "utils/quadrature.h"
 
@@ -9,14 +10,29 @@ Real legendre0(const Real x)
   return 1;
 }
 
+Real legendre0_dx(const Real x)
+{
+  return 0;
+}
+
 Real legendre1(const Real x)
 {
   return x;
 }
 
+Real legendre1_dx(const Real x)
+{
+  return 1;
+}
+
 Real legendre2(const Real x)
 {
-  return 0.5*(3*x*x -1);
+  return 0.5*(3*x*x - 1);
+}
+
+Real legendre2_dx(const Real x)
+{
+  return 0.5*(6*x);
 }
 
 Real legendre3(const Real x)
@@ -24,11 +40,20 @@ Real legendre3(const Real x)
   return 0.5*(5*std::pow(x, 3) - 3*x);
 }
 
+Real legendre3_dx(const Real x)
+{
+  return 0.5*(15*std::pow(x, 2) - 3);
+}
+
 Real legendre4(const Real x)
 {
   return 0.125*(35*std::pow(x, 4) - 30*std::pow(x, 2) + 3);
 }
 
+Real legendre4_dx(const Real x)
+{
+  return 0.125*(4*35*std::pow(x, 3) - 2*30*x);
+}
 
 
 
@@ -42,23 +67,132 @@ TEST(Polynomials, Legendre)
 
   // p = 0
   for (int i=0; i < nvals; ++i)
+  {
     EXPECT_FLOAT_EQ(legendre.evalPoly(0, xvals[i]), legendre0(xvals[i]));
+    EXPECT_FLOAT_EQ(legendre.evalPolyDeriv(0, xvals[i]), legendre0_dx(xvals[i]));
+  }
 
   // p = 1
   for (int i=0; i < nvals; ++i)
+  {
     EXPECT_FLOAT_EQ(legendre.evalPoly(1, xvals[i]), legendre1(xvals[i]));
+    EXPECT_FLOAT_EQ(legendre.evalPolyDeriv(1, xvals[i]), legendre1_dx(xvals[i]));
+  }
 
   // p = 2
   for (int i=0; i < nvals; ++i)
+  {
     EXPECT_FLOAT_EQ(legendre.evalPoly(2, xvals[i]), legendre2(xvals[i]));
+    EXPECT_FLOAT_EQ(legendre.evalPolyDeriv(2, xvals[i]), legendre2_dx(xvals[i]));
+  }
 
   // p = 3
   for (int i=0; i < nvals; ++i)
+  {
     EXPECT_FLOAT_EQ(legendre.evalPoly(3, xvals[i]), legendre3(xvals[i]));
+    EXPECT_FLOAT_EQ(legendre.evalPolyDeriv(3, xvals[i]), legendre3_dx(xvals[i]));
+  }
 
   // p = 4
   for (int i=0; i < nvals; ++i)
+  {
     EXPECT_FLOAT_EQ(legendre.evalPoly(4, xvals[i]), legendre4(xvals[i]));
+    EXPECT_FLOAT_EQ(legendre.evalPolyDeriv(4, xvals[i]), legendre4_dx(xvals[i]));
+  }
+}
+
+
+const std::vector<Real> pts = {-1, -0.5, 0.5, 1};
+
+Real lagrange0(const Real x)
+{
+  Real xi = pts[0];
+  return (x - pts[1])*(x - pts[2])*(x - pts[3]) /( (xi - pts[1])*(xi - pts[2])*(xi - pts[3]) );
+}
+
+Real lagrange0_dx(const Real x)
+{
+  Real xi = pts[0];
+  Real den = (xi - pts[1])*(xi - pts[2])*(xi - pts[3]);
+  Real num = (x - pts[2])*(x - pts[3]) + (x - pts[1])*(x - pts[3]) + (x - pts[1])*(x - pts[2]);
+
+  return num / den;
+}
+
+Real lagrange1(const Real x)
+{
+  Real xi = pts[1];
+  return (x - pts[0])*(x - pts[2])*(x - pts[3]) /( (xi - pts[0])*(xi - pts[2])*(xi - pts[3]) );
+}
+
+Real lagrange1_dx(const Real x)
+{
+  Real xi = pts[1];
+  Real den = (xi - pts[0])*(xi - pts[2])*(xi - pts[3]);
+  Real num = (x - pts[2])*(x - pts[3]) + (x - pts[0])*(x - pts[3]) + (x - pts[0])*(x - pts[2]);
+
+  return num / den;
+}
+
+Real lagrange2(const Real x)
+{
+  Real xi = pts[2];
+  return (x - pts[0])*(x - pts[1])*(x - pts[3]) /( (xi - pts[0])*(xi - pts[1])*(xi - pts[3]) );
+}
+
+Real lagrange2_dx(const Real x)
+{
+  Real xi = pts[2];
+  Real den = (xi - pts[0])*(xi - pts[1])*(xi - pts[3]);
+  Real num = (x - pts[1])*(x - pts[3]) + (x - pts[0])*(x - pts[3]) + (x - pts[0])*(x - pts[1]);
+
+  return num / den;
+}
+
+Real lagrange3(const Real x)
+{
+  Real xi = pts[3];
+  return (x - pts[0])*(x - pts[1])*(x - pts[2]) /( (xi - pts[0])*(xi - pts[1])*(xi - pts[2]) );
+}
+
+Real lagrange3_dx(const Real x)
+{
+  Real xi = pts[3];
+  Real den = (xi - pts[0])*(xi - pts[1])*(xi - pts[2]);
+  Real num = (x - pts[1])*(x - pts[2]) + (x - pts[0])*(x - pts[2]) + (x - pts[0])*(x - pts[1]);
+
+  return num / den;
+}
+
+TEST(Polynomials, Lagrange)
+{
+  std::vector<Real> pts_test = {-2, -1, -0.75, -0.5, 0.25, 0, 0.25, 0.5, 0.75, 1, 1};
+  LagrangeBasis lagrange(pts);
+
+  // j = 0
+  for (auto pts_i : pts_test)
+  {
+    EXPECT_FLOAT_EQ(lagrange.evalPoly(0, pts_i), lagrange0(pts_i));
+    EXPECT_FLOAT_EQ(lagrange.evalPolyDeriv(0, pts_i), lagrange0_dx(pts_i));
+  }
+
+  for (auto pts_i : pts_test)
+  {
+    EXPECT_FLOAT_EQ(lagrange.evalPoly(1, pts_i), lagrange1(pts_i));
+    EXPECT_FLOAT_EQ(lagrange.evalPolyDeriv(1, pts_i), lagrange1_dx(pts_i));
+  }
+
+  for (auto pts_i : pts_test)
+  {
+    EXPECT_FLOAT_EQ(lagrange.evalPoly(2, pts_i), lagrange2(pts_i));
+    EXPECT_FLOAT_EQ(lagrange.evalPolyDeriv(2, pts_i), lagrange2_dx(pts_i));
+  }
+
+  for (auto pts_i : pts_test)
+  {
+    EXPECT_FLOAT_EQ(lagrange.evalPoly(3, pts_i), lagrange3(pts_i));
+    EXPECT_FLOAT_EQ(lagrange.evalPolyDeriv(3, pts_i), lagrange3_dx(pts_i));
+  }
 }
 
 TEST(Polynomials, Quadrature)
