3f77571d55305bb087526ad7fa572cf159c5275d
Jared Crean <jcrean01@gmail.com>
2022-07-15 17:36:45 -0400

    move functions to source files

--- include/physics/heat/sky_radiation.h
+++ include/physics/heat/sky_radiation.h
@@ -23,46 +23,9 @@ class SkyRadiationModel
     // set the air temperature (which the model uses as the temperature of the ground)
     void setAirTemperature(Real t_air) { m_t_air4 = std::pow(t_air, 4); }
 
-    Real computeFlux(Real t_surf, const std::array<Real, 3>& unit_normal)
-    {
-      Real cos_phi = computeCosTiltAngle(unit_normal);
-      Real beta     = std::sqrt(0.5*(1 + cos_phi));
+    Real computeFlux(Real t_surf, const std::array<Real, 3>& unit_normal);
 
-      Real f_ground = 0.5 * (1 - cos_phi);
-      Real f_sky    = beta * 0.5 * (1 + cos_phi);
-      Real f_air    = (1 - beta) * 0.5 * (1 + cos_phi);
-
-      Real t_surf4   = std::pow(t_surf, 4);
-      Real t_ground4 = m_t_air4;
-
-      Real flux = m_emittance * m_sigma * ( f_ground * (t_ground4 - t_surf4) +
-                                            f_sky    * (m_t_sky4  - t_surf4) +
-                                            f_air    * (m_t_air4  - t_surf4) );
-      return flux;
-    }
-
-    Real computeFluxDeriv(Real t_surf, const std::array<Real, 3>& unit_normal, Real& flux_dot)
-    {
-      Real cos_phi = computeCosTiltAngle(unit_normal);
-      Real beta     = std::sqrt(0.5*(1 + cos_phi));
-
-      Real f_ground = 0.5 * (1 - cos_phi);
-      Real f_sky    = beta * 0.5 * (1 + cos_phi);
-      Real f_air    = (1 - beta) * 0.5 * (1 + cos_phi);
-
-      Real t_surf4   = std::pow(t_surf, 4);
-      Real t_surf4_dot = 4 * std::pow(t_surf, 3);
-      Real t_ground4 = m_t_air4;
-
-      Real flux = m_emittance * m_sigma * ( f_ground * (t_ground4 - t_surf4) +
-                                            f_sky    * (m_t_sky4  - t_surf4) +
-                                            f_air    * (m_t_air4  - t_surf4) );
-
-      flux_dot   = m_emittance * m_sigma * ( f_ground * (-t_surf4_dot) +
-                                             f_sky    * (-t_surf4_dot) +
-                                             f_air    * (-t_surf4_dot) );
-      return flux;
-    }
+    Real computeFluxDeriv(Real t_surf, const std::array<Real, 3>& unit_normal, Real& flux_dot);
 
   private:
 
--- include/physics/heat/tarp.h
+++ include/physics/heat/tarp.h
@@ -58,80 +58,15 @@ class TarpModel
       return roughness_factors[roughness_index];
     }
 
-    Real computeHeatTransferCoeff(Real wall_temp, const std::array<Real, 3>& pt, const std::array<Real, 3>& unit_normal)
-    {
-      Real delta_t = m_air_temp - wall_temp;
-      Real air_speed = computeLocalWindSpeed(pt);
-      bool is_windward = dot(unit_normal, m_air_direction) < 0;
-
-      Real Wf = is_windward ? 1.0 : 0.5;
-
-      Real h_f = 2.537 * Wf * m_roughness_fac * std::sqrt(m_surface_perimeter * air_speed/m_surface_area);
-      Real cos_eps = computeCosTiltAngle(unit_normal);
-      Real delta_t_one_third = std::pow(std::abs(delta_t), 1.0/3.0);
-
-      Real h_n = 0.0;
-      if ((delta_t < 0 && cos_eps > 0) || (delta_t > 0 && cos_eps < 0)) {
-        h_n = 9.482 * delta_t_one_third / (7.283 - std::abs(cos_eps));
-      } else if ( (delta_t > 0 && cos_eps > 0) || (delta_t < 0 && cos_eps < 0)) {
-        h_n = 1.810 * delta_t_one_third/ (1.382 + std::abs(cos_eps));
-      } else {
-        h_n = 1.31 * delta_t_one_third;
-      }
-
-      return h_f + h_n;
-    }
+    Real computeHeatTransferCoeff(Real wall_temp, const std::array<Real, 3>& pt, const std::array<Real, 3>& unit_normal);
 
     // returns the heat transfer coefficient, and h_n_dot is overwritten with its derivative
     // with respect to the surface temperature
-    Real computeHeatTransferCoeffDeriv(Real wall_temp, const std::array<Real, 3>& pt, const std::array<Real, 3>& unit_normal, Real& h_n_dot)
-    {
-      Real delta_t = m_air_temp - wall_temp;
-      Real delta_t_dot = -1;
+    Real computeHeatTransferCoeffDeriv(Real wall_temp, const std::array<Real, 3>& pt, const std::array<Real, 3>& unit_normal, Real& h_n_dot);
 
-      Real air_speed = computeLocalWindSpeed(pt);
-      bool is_windward = dot(unit_normal, m_air_direction) < 0;
+    Real computeCosTiltAngle(const std::array<Real, 3>& unit_normal);
 
-      Real Wf = is_windward ? 1.0 : 0.5;
-      Real h_f = 2.537 * Wf * m_roughness_fac * std::sqrt(m_surface_perimeter * air_speed/m_surface_area);
-      Real cos_eps = computeCosTiltAngle(unit_normal);
-
-      Real abs_delta_t = std::abs(delta_t);  
-      Real abs_delta_t_dot = delta_t > 0 ? delta_t_dot : -delta_t_dot;
-
-      Real delta_t_one_third = std::pow(abs_delta_t, 1.0/3.0);
-      Real delta_t_one_third_dot = (1.0/3.0) * std::pow(abs_delta_t, -2.0/3.0) * abs_delta_t_dot;
-
-      Real h_n = 0.0;
-      h_n_dot = 0.0;
-      if ((delta_t < 0 && cos_eps > 0) || (delta_t > 0 && cos_eps < 0))
-      {
-        h_n     = 9.482 * delta_t_one_third     / (7.283 - std::abs(cos_eps));
-        h_n_dot = 9.482 * delta_t_one_third_dot / (7.283 - std::abs(cos_eps));
-      } else if ( (delta_t > 0 && cos_eps > 0) || (delta_t < 0 && cos_eps < 0))
-      {
-        h_n     = 1.810 * delta_t_one_third     / (1.382 + std::abs(cos_eps));
-        h_n_dot = 1.810 * delta_t_one_third_dot / (1.382 + std::abs(cos_eps));
-      } else
-      {
-        h_n     = 1.31 * delta_t_one_third;
-        h_n_dot = 1.31 * delta_t_one_third_dot;
-      }
-
-      return h_f + h_n;
-    }
-
-    Real computeCosTiltAngle(const std::array<Real, 3>& unit_normal)
-    {
-      return dot(unit_normal, m_vertical_vector);
-    }
-
-    Real computeLocalWindSpeed(const std::array<Real, 3>& pt)
-    {
-      auto r = pt - m_point_at_zero_altitude;
-      auto local_height = dot(r, m_vertical_vector);
-      return m_local_wind_velocity_calc.computeLocalVelocity(m_air_speed, local_height);
-    }
+    Real computeLocalWindSpeed(const std::array<Real, 3>& pt);
 
   private:
     Real m_air_temp     = std::numeric_limits<Real>::min();
