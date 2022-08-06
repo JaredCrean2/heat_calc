a68c7da86c1e5cbaf51852b78a8db1863a1e2ca3
Jared Crean <jcrean01@gmail.com>
2022-08-06 13:20:12 -0400

    remove incorrect assert

--- src/linear_system/large_matrix.cc
+++ src/linear_system/large_matrix.cc
@@ -6,8 +6,6 @@ void solve(LargeMatrixPtr mat, const DiscVectorPtr b, DiscVectorPtr x)
 {
   auto& b_vec = b->getVector();
   auto& x_vec = x->getVector();
-  assert(b_vec.shape()[0] == mat->getMLocal());
-  assert(x_vec.shape()[0] == mat->getMLocal());
 
   mat->solve(b_vec, x_vec);
 }
