3dd49aab28b1ae0f729c41401c78ab6ee8a63b41
Jared Crean <jcrean01@gmail.com>
2023-01-03 15:32:40 -0500

    add derivative test for window conduction model

--- test/test_window_conduction_model.cc
+++ test/test_window_conduction_model.cc
@@ -1,6 +1,23 @@
 #include "gtest/gtest.h"
 #include "physics/heat/window_conduction_model.h"
 
+namespace {
+
+void test_finite_difference(Heat::WindowConductionModel& model, Real t_interior, Real t_exterior)
+{
+  Real eps = 1e-7;
+  Real flux1 = model.computeConductionPower(t_interior, t_exterior);
+  Real flux2 = model.computeConductionPower(t_interior + eps, t_exterior);
+  Real deriv_fd = (flux2 - flux1)/eps;
+
+  Real deriv_ad;
+  Real flux3 = model.computeConductionPowerDot(t_interior, t_exterior, deriv_ad);
+
+  EXPECT_NEAR(flux1, flux3, 1e-13);
+  EXPECT_NEAR(deriv_fd, deriv_ad, 1e-6);
+}
+}
+
 TEST(WindowConductionModel, Value)
 {
   Real r_val = 2;
@@ -11,4 +28,5 @@ TEST(WindowConductionModel, Value)
   Heat::WindowConductionModel model(r_val, area);
 
   EXPECT_EQ(model.computeConductionPower(t_interior, t_exterior), (t_exterior - t_interior)*area/r_val);
+  test_finite_difference(model, t_interior, t_exterior);
 }
